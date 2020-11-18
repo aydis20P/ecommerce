cf61cda70d7bafee1d00a763e81a670602f484af
williampalacios <mich.palacios.hdz@gmail.com>
2020-11-17 19:51:05 -0600

    models rev

--- store/models.py
+++ store/models.py
@@ -17,7 +17,7 @@ class Item(models.Model):
 	name = models.CharField(max_length=45)
 	description = models.CharField(max_length=45)
 	price = models.FloatField()
-	image = models.CharField(max_length=45)
+	#image = models.CharField(max_length=45)
 
 	def __str__(self):
 		return self.name
@@ -31,17 +31,17 @@ class Order(models.Model):
 class OrderItem(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.CASCADE)
 	item = models.ForeignKey(Item, on_delete=models.CASCADE)
-	quantity = models.IntegerField(default=0)
+	quantity = models.IntegerField(default=0, null=True, blank=True)
 	dateTimeOI = models.DateTimeField(auto_now_add=True)
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
 
 
 	def __str__(self):
-		return str(self.order + self.item)
+		return self.order + self.item
 
 class UserOrder(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 	order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return str(self.customer + self.order)
+		return self.customer + self.order
